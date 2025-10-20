5c5c7af92987b16c2c4c02e3712cabd7e8bbfb5e
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2025-10-20 08:42:54 +0530

    Fix backend for Vercel serverless

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/api', (req, res) => {
 
 app.use('/api', informationRouter);
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("server is running on port 3000");
+// });
+module.exports = app;
