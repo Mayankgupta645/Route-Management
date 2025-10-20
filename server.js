30cfa8322965a10b8f210af9b21f79a2e75e729d
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2025-10-20 08:31:35 +0530

    Fixed /api prefix for Vercel deployment

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const bodyParser = require('body-parser');
 app.use(cors());  
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send("welcome to the travel agency price telling");
 });
 
-app.use('/', informationRouter);
+app.use('/api', informationRouter);
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
